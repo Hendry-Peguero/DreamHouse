0627bedd4e22b41713473bc00a55a1e19c7124dc
Rafael Gomez <rafaelogomezr@gmail.com>
2024-08-07 23:49:10 -0400

    Ya el mediator y CQRS esta implementado, con sus dependencias necesarias, Property tiene un solo endpoint, GetAllWithInclude pero el include solo trae el typo de propiedad de lo contrario da error

---
+++ DreamHouse.Core.Application/Features/Properties/Queries/GetAllProperties/GetAllPropertiesQuery.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DreamHouse.Core.Application.Interfaces.Repositories;
+using DreamHouse.Core.Application.ViewModels.Property;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamHouse.Core.Application.Features.Properties.Queries.GetAllProperties
+{
+    public class GetAllPropertiesQuery : IRequest<List<PropertyViewModel>>
+    {
+    }
+
+    public class GetAllPropertiesQueryHandler : IRequestHandler<GetAllPropertiesQuery, List<PropertyViewModel>>
+    {
+        private readonly IPropertyRepository propertyRepository;
+        private readonly IMapper mapper;
+
+        public GetAllPropertiesQueryHandler(IPropertyRepository propertyRepository , IMapper mapper)
+        {
+            this.propertyRepository = propertyRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<PropertyViewModel>> Handle(GetAllPropertiesQuery request, CancellationToken cancellationToken)
+        {
+
+            var propertiesVm = mapper.Map<List<PropertyViewModel>>(await propertyRepository.GetAllWithIncludeAsync(new List<string> {"TypeProperty"}));
+            if (propertiesVm == null) throw new Exception("There are not categories");
+
+            return propertiesVm;
+        }
+    }
+
+}
---
+++ WebApi.DreamHouse/Controllers/v1/PropertyController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using DreamHouse.Core.Application.Features.Properties.Queries.GetAllProperties;
+using DreamHouse.Core.Application.ViewModels.Property;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.DreamHouse.Controllers.General;
+
+namespace WebApi.DreamHouse.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Authorize(Roles = "ADMIN,DEVELOPER" )]
+    public class PropertyController : BaseApiController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<PropertyViewModel>))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await Mediator.Send(new GetAllPropertiesQuery()));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
